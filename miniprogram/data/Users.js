e656f05a6b8774b9511fbb7d3b74fb2b77906fef
Huang Yufei <60833761+Kirito-Ausna@users.noreply.github.com>
2021-01-10 17:48:19 +0800

    AnalysisForm

---
+++ miniprogram/data/Users.js
@@ -0,0 +1,14 @@
+var Users=[
+  {
+    PictureUrl:"https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1438273599,2608288512&fm=26&gp=0.jpg",
+    name:"屈臣氏"
+  },
+  {
+    PictureUrl:"https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=72303662,2399209959&fm=26&gp=0.jpg",
+    name:"香奈儿"
+  },
+]
+
+module.exports={
+  Users: Users
+}
