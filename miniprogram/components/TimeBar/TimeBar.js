4d4330ffdf7a40f47c0847c835a81d634795fa0a
Wanshe <1124864321@qq.com>
2021-01-10 12:59:42 +0800

    修改Analysis.wxss，开始写TimeBar，TimePoint

--- miniprogram/components/TimeBar/TimeBar.js
+++ miniprogram/components/TimeBar/TimeBar.js
@@ -11,13 +11,50 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    particle:[{'q':'a'}],
+    array: [{
+      message: 'foo',
+    }, {
+      message: 'bar'
+    }]
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
+  },
+  
+  lifetimes:{
+    // created: function(e){
+    //   console.log("TimeBar is created.")
+    attached: function(e){
+      console.log("TimeBar is attached.")
+      var i,temp=[];
+      for( i=0; i<24; i++){
+        temp.push({
+          idx: i,
+          density: 0
+        })
+      }
+      this.setData({
+        particle: temp
+      })
+      console.log(this.data.particle)
+    }
+  },
 
+  pageLifetimes:{
+    // show: function(e){
+    //   console.log("TimeBar of page shows.")
+    //   var i;
+    //   for( i=0; i<24; i++){
+    //     this.data.particle.push({
+    //       idx: i,
+    //       density: 0
+    //     })
+    //   }
+    //   console.log(this.data.particle)
+    // }
   }
 })
